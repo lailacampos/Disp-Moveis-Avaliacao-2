cefdab8d948a552c1a1b86ce45da1766352ca4f1
Laila Campos <enders.game1990@gmail.com>
2021-09-11 14:24:07 -0300

    Add layout for configuration screen

---
+++ app/src/main/java/com/laila/unifacs/avaliacao1/activity/ConfigScreen.java
@@ -0,0 +1,16 @@
+package com.laila.unifacs.avaliacao1.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.laila.unifacs.avaliacao1.R;
+
+public class ConfigScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_screen);
+    }
+}
--- app/src/main/java/com/laila/unifacs/avaliacao1/activity/MainActivity.java
+++ app/src/main/java/com/laila/unifacs/avaliacao1/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.laila.unifacs.avaliacao1;
+package com.laila.unifacs.avaliacao1.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.laila.unifacs.avaliacao1.R;
 
 public class MainActivity extends AppCompatActivity {
 
