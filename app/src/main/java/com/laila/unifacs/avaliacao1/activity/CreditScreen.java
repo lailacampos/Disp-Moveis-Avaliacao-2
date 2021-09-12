9c5f791232feaf4a77ace3c500fc942ff076f269
Laila Campos <enders.game1990@gmail.com>
2021-09-12 07:45:31 -0300

    Add credit layout

---
+++ app/src/main/java/com/laila/unifacs/avaliacao1/activity/CreditScreen.java
@@ -0,0 +1,16 @@
+package com.laila.unifacs.avaliacao1.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.laila.unifacs.avaliacao1.R;
+
+public class CreditScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_screen);
+    }
+}
