35aaf57fc542a34a5604d6b1fc8948d3ca9a6e83
Laila Campos <enders.game1990@gmail.com>
2021-09-18 11:53:44 -0300

    Delete unused RadioButton variables

--- app/src/main/java/com/laila/unifacs/avaliacao1/activity/ConfigScreen.java
+++ app/src/main/java/com/laila/unifacs/avaliacao1/activity/ConfigScreen.java
@@ -20,12 +20,6 @@ public class ConfigScreen extends AppCompatActivity {
     private RadioGroup tipoMapaRadioGroup;
     private RadioGroup infoTrafegoRadioGroup;
 
-    private RadioButton grauDecimalRadionButton, grauMinutoRadionButton, grauSegundoRadionButton;
-    private RadioButton quilometroHoraRadionButton, milhaHoraRadionButton;
-    private RadioButton nenhumaRadionButton, northUpRadionButton, courseUpRadionButton;
-    private RadioButton vetorialRadionButton, sateliteRadionButton;
-    private RadioButton ligadoRadionButton, desligadoRadionButton;
-
     private int coordGeograficasRadionButtonSelected;
     private int unidadeVelocidadeRadioButtonSelected;
     private int orientacaoMapaRadioButtonSelected;
@@ -55,23 +49,6 @@ public class ConfigScreen extends AppCompatActivity {
         this.orientacaoMapaRadioGroup =     findViewById(R.id.orientacao_mapa_RadioGroup);
         this.tipoMapaRadioGroup =           findViewById(R.id.tipo_mapa_RadioGroup);
         this.infoTrafegoRadioGroup =        findViewById(R.id.informacoes_trafego_RadioGroup);
-
-        this.grauDecimalRadionButton =      findViewById(R.id.grau_decimal_RadioButton);
-        this.grauMinutoRadionButton =       findViewById(R.id.grau_minuto_RadioButton);
-        this.grauSegundoRadionButton =      findViewById(R.id.grau_minuto_segundo_RadioButton);
-
-        this.quilometroHoraRadionButton =   findViewById(R.id.km_hora_RadioButton);
-        this.milhaHoraRadionButton =        findViewById(R.id.milha_hora_RadioButton);
-
-        this.nenhumaRadionButton =          findViewById(R.id.orientacao_nenhuma_RadioButton);
-        this.northUpRadionButton =          findViewById(R.id.orientacao_north_up_RadioButton);
-        this.courseUpRadionButton =         findViewById(R.id.orientacao_course_up_RadioButton);
-
-        this.vetorialRadionButton =         findViewById(R.id.vetorial_RadioButton);
-        this.sateliteRadionButton =         findViewById(R.id.imagem_satelite_RadioButton);
-
-        this.ligadoRadionButton =           findViewById(R.id.trafego_ligado_RadioButton);
-        this.desligadoRadionButton =        findViewById(R.id.trafego_desligado_RadioButton);
 
     }
 
