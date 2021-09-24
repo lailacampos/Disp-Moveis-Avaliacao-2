bc6d0f6885f7eed0652c7c9cdf90882bc2c9e69c
Laila Campos <enders.game1990@gmail.com>
2021-09-24 10:14:22 -0300

    Change appearance of Config Screen. Add icons to Config Screen. Add comments to Config Acticity.

--- app/src/main/java/com/laila/unifacs/avaliacao1/activity/ConfigScreen.java
+++ app/src/main/java/com/laila/unifacs/avaliacao1/activity/ConfigScreen.java
@@ -44,6 +44,7 @@ public class ConfigScreen extends AppCompatActivity {
 
     public void initViews() {
 
+        // Inicia as views com o método findViewById()
         this.coordGeograficasRadioGroup =   findViewById(R.id.coordenadas_geograficas_RadioGroup);
         this.unidadeVelocidadeRadioGroup =  findViewById(R.id.unidade_velocidade_RadioGroup);
         this.orientacaoMapaRadioGroup =     findViewById(R.id.orientacao_mapa_RadioGroup);
@@ -54,8 +55,12 @@ public class ConfigScreen extends AppCompatActivity {
 
     public void getPreferencesData() {
 
+        // Recovers the sharedPreferences object that contains the saved preference table by passing the preference name
+        // Recupera o objeto sharedPreferences que contém a tabela com preferências salvas através do nome da preferência
         this.sharedPreferences = getSharedPreferences(this.PREFERENCE_NAME, 0);
 
+        // Recovers the selected radioButton id of each RadioGroup
+        // Recupera o id do radioButton selecionado de cada RadioGroup
         this.coordGeograficasRadionButtonSelected = this.sharedPreferences.getInt(this.COORD_GEO_KEY, 0);
         this.unidadeVelocidadeRadioButtonSelected = this.sharedPreferences.getInt(this.UNIDADE_VELOCIDADE_KEY, 0);
         this.orientacaoMapaRadioButtonSelected = this.sharedPreferences.getInt(this.ORIENTACAO_MAPA_KEY, 0);
@@ -63,6 +68,7 @@ public class ConfigScreen extends AppCompatActivity {
         this.infoTrafegoRadioButtonSelected = this.sharedPreferences.getInt(this.INFO_TRAFEGO_KEY, 0);
 
         // Checks the button specified by id and unchecks the others in a RadioGroup
+        // Seleciona o RadioButton especificado e deseleciona os outros dentro de um RadioGroup
         this.coordGeograficasRadioGroup.check(this.coordGeograficasRadionButtonSelected);
         this.unidadeVelocidadeRadioGroup.check(this.unidadeVelocidadeRadioButtonSelected);
         this.orientacaoMapaRadioGroup.check(this.orientacaoMapaRadioButtonSelected);
@@ -74,6 +80,7 @@ public class ConfigScreen extends AppCompatActivity {
     public void savePreferences(View view) {
 
         // Get the id of the chosen RadioButton of each RadioGroup
+        // Recupera o id do RadioButton selecionado dentro de cada RadioGroup
         this.coordGeograficasRadionButtonSelected = this.coordGeograficasRadioGroup.getCheckedRadioButtonId();
         this.unidadeVelocidadeRadioButtonSelected = this.unidadeVelocidadeRadioGroup.getCheckedRadioButtonId();
         this.orientacaoMapaRadioButtonSelected = this.orientacaoMapaRadioGroup.getCheckedRadioButtonId();
@@ -81,6 +88,7 @@ public class ConfigScreen extends AppCompatActivity {
         this.infoTrafegoRadioButtonSelected = this.infoTrafegoRadioGroup.getCheckedRadioButtonId();
 
         // Save data with SharedPreferences
+        // Salva os dados com SharedPreferences
         SharedPreferences.Editor editor = this.sharedPreferences.edit();
         editor.putInt(COORD_GEO_KEY, coordGeograficasRadionButtonSelected);
         editor.putInt(UNIDADE_VELOCIDADE_KEY, unidadeVelocidadeRadioButtonSelected);
@@ -93,6 +101,8 @@ public class ConfigScreen extends AppCompatActivity {
 
     public void clearPreferences(View view) {
 
+        // Clear all selected RadioButtons
+        // Limpa todas as seleções
         this.coordGeograficasRadioGroup.clearCheck();
         this.unidadeVelocidadeRadioGroup.clearCheck();
         this.orientacaoMapaRadioGroup.clearCheck();
